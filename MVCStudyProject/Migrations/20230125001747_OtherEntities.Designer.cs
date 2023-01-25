b00c08a5276a7b4e5a417395977fa8b23d8ac1d7
WilliCarvalho <wcarvalho@kokku.com.br>
2023-01-24 21:26:47 -0300

    Added models and DB migration

--- MVCStudyProject/Migrations/20230125001747_OtherEntities.Designer.cs
+++ MVCStudyProject/Migrations/20230125001747_OtherEntities.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MVCStudyProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVCStudyProject.Migrations
 {
     [DbContext(typeof(MVCStudyProjectContext))]
-    partial class MVCStudyProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20230125001747_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
