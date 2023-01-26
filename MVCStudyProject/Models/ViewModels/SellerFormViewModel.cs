73fdc99511fbeb819074158e9dd08df70ff585b6
WilliCarvalho <wcarvalho@kokku.com.br>
2023-01-26 19:14:53 -0300

    SellerFormViewModel creation/implementation

---
+++ MVCStudyProject/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MVCStudyProject.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
---
+++ MVCStudyProject/Services/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using MVCStudyProject.Data;
+using MVCStudyProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MVCStudyProject.Services
+{
+    public class DepartmentService
+    {
+        private readonly MVCStudyProjectContext _context;
+
+        public DepartmentService(MVCStudyProjectContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
